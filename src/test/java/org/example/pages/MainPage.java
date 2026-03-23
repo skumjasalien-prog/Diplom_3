a3b3f9dca9516b525d3a71ed467edfdab2e36e94
skumjasalien-prog <skumjasalien@gmail.com>
2026-03-24 00:29:04 +0300

    исправила ошибки добавила алюр

--- src/test/java/org/example/pages/MainPage.java
+++ src/test/java/org/example/pages/MainPage.java
@@ -4,6 +4,7 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -22,6 +23,7 @@ public class MainPage {
     private final By bunsTab = By.xpath("//div[contains(@class,'tab_tab') and .//span[normalize-space(.)='Булки']]");
     private final By saucesTab = By.xpath("//div[contains(@class,'tab_tab') and .//span[normalize-space(.)='Соусы']]");
     private final By fillingsTab = By.xpath("//div[contains(@class,'tab_tab') and .//span[normalize-space(.)='Начинки']]");
+    private final By modalOverlay = By.cssSelector(".Modal_modal_overlay__x2ZCr");
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
@@ -47,20 +49,26 @@ public class MainPage {
 
     @Step("Перейти во вкладку 'Булки'")
     public void openBunsTab() {
-        driver.findElement(bunsTab).click();
+        clickTabWhenReady(bunsTab);
         wait.until(d -> isCurrentTab("Булки"));
     }
 
     @Step("Перейти во вкладку 'Соусы'")
     public void openSaucesTab() {
-        driver.findElement(saucesTab).click();
+        clickTabWhenReady(saucesTab);
         wait.until(d -> isCurrentTab("Соусы"));
     }
 
     @Step("Перейти во вкладку 'Начинки'")
     public void openFillingsTab() {
-        driver.findElement(fillingsTab).click();
+        clickTabWhenReady(fillingsTab);
         wait.until(d -> isCurrentTab("Начинки"));
+    }
+
+    private void clickTabWhenReady(By tabLocator) {
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(modalOverlay));
+        WebElement tab = wait.until(ExpectedConditions.elementToBeClickable(tabLocator));
+        tab.click();
     }
 
     private By currentTabText(String tabText) {
